c8793c9e17dc5dee0d400d514e364da2a8de2f3c
Link Wu <1774241372@qq.com>
2021-03-01 22:09:48 +0800

    rqt does not work on ARM

--- src/ubuntu/install/tigervnc.sh
+++ src/ubuntu/install/tigervnc.sh
@@ -3,5 +3,4 @@ set -e
 
 echo "Install TigerVNC server"
 # wget -qO- https://dl.bintray.com/tigervnc/stable/tigervnc-1.8.0.x86_64.tar.gz | tar xz --strip 1 -C /
-
-sudo apt install tigervnc-standalone-server -y
+sudo apt install tightvncserver -y
