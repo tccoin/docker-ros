fb26622b082e3b74fba1ffa77348db6ded142e5c
Tccoin <coin12.wu@gmail.com>
2021-03-03 16:34:19 +0800

    fix minor bugs

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 architecture=$(dpkg --print-architecture)
-if [[ $architecture=="arm64" ]]; then
+echo "Building image for ${architecture}..."
+if [[ $architecture == "arm64" ]]; then
     docker pull arm64v8/ros:noetic-perception
     docker tag arm64v8/ros:noetic-perception tccoin/docker-ros:ros
     docker build -t tccoin/docker-ros --file Dockerfile.arm64 .
-elif [[ $architecture=="amd64" ]]; then
+elif [[ $architecture == "amd64" ]]; then
     docker pull osrf/ros:noetic-desktop-full
     docker tag osrf/ros:noetic-desktop-full tccoin/docker-ros:ros
     docker build -t tccoin/docker-ros --file Dockerfile.amd64 .
--- pull.sh
+++ pull.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 architecture=$(dpkg --print-architecture)
-if [[ $architecture=="arm64" ]]; then
+echo "Pulling image for ${architecture}..."
+if [[ $architecture == "arm64" ]]; then
     docker pull arm64v8/ros:noetic-perception
     docker tag arm64v8/ros:noetic-perception tccoin/docker-ros:ros
     docker build -t tccoin/docker-ros --file Dockerfile.arm64 .
     docker build -t tccoin/docker-ros:rosbridge webviz
-elif [[ $architecture=="amd64" ]]; then
+elif [[ $architecture == "amd64" ]]; then
     docker pull osrf/ros:noetic-desktop-full
     docker tag osrf/ros:noetic-desktop-full tccoin/docker-ros:ros
     docker pull tccoin/docker-ros:latest
