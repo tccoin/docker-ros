041adeb44edb321d3680768379dd79021ed563da
Tccoin <coin12.wu@gmail.com>
2021-03-18 03:01:54 +0800

    不再使用rosbridge标签

--- build.sh
+++ build.sh
@@ -1,16 +1,12 @@
 #!/usr/bin/env bash
+
 architecture=$(dpkg --print-architecture)
 echo "Building image for ${architecture}..."
+
 if [[ $architecture == "arm64" ]]; then
-    docker pull arm64v8/ros:noetic-perception
-    docker tag arm64v8/ros:noetic-perception tccoin/docker-ros:ros
     docker build -t tccoin/docker-ros --file Dockerfile.arm64 .
 elif [[ $architecture == "amd64" ]]; then
-    docker pull osrf/ros:noetic-desktop-full
-    docker tag osrf/ros:noetic-desktop-full tccoin/docker-ros:ros
     docker build -t tccoin/docker-ros --file Dockerfile.amd64 .
-    docker build -t tccoin/docker-ros:rosbridge --file webviz/Dockerfile .
 fi
 
-docker build -t tccoin/docker-ros:rosbridge webviz
 # docker build -t tccoin/docker-ros:kinect kinect
--- install.sh
+++ install.sh
@@ -1,3 +1,13 @@
-docker pull osrf/ros:noetic-desktop-full
+# pull images
+docker pull cruise/webviz
+if [[ $architecture == "arm64" ]]; then
+    docker pull arm64v8/ros:noetic-perception
+    docker tag arm64v8/ros:noetic-perception tccoin/docker-ros:ros
+elif [[ $architecture == "amd64" ]]; then
+    docker pull osrf/ros:noetic-desktop-full
+    docker tag osrf/ros:noetic-desktop-full tccoin/docker-ros:ros
+fi
+
+# create workspace
 touch ws/zsh/.zsh_history
 git clone https://github.com/RoboVigor/RV-AutoAim ws/autoaim
