bbcebe2f1cea93f14c3198bfb86dc089d56f2b90
Tccoin <coin12.wu@gmail.com>
2021-03-18 02:56:04 +0800

    合并镜像, 修复小觅相机官方源bug

--- build.sh
+++ build.sh
@@ -9,6 +9,7 @@ elif [[ $architecture == "amd64" ]]; then
     docker pull osrf/ros:noetic-desktop-full
     docker tag osrf/ros:noetic-desktop-full tccoin/docker-ros:ros
     docker build -t tccoin/docker-ros --file Dockerfile.amd64 .
+    docker build -t tccoin/docker-ros:rosbridge --file webviz/Dockerfile .
 fi
 
 docker build -t tccoin/docker-ros:rosbridge webviz
